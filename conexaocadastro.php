9aa63f10202a4b03f7949a6de2431fcb73fba0e0
AndreyRodriguesPantoja <andrey.rodrigues1998@gmail.com>
2026-06-16 20:15:14 -0300

    Fiz a tela de cadastro e conexao do php

---
+++ conexaocadastro.php
@@ -0,0 +1,74 @@
+<?php
+
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "sad_superseguro";
+
+$conexao = mysqli_connect(
+    $host,
+    $usuario,
+    $senha,
+    $banco
+);
+
+if(!$conexao){
+    die("Erro de conexão");
+}
+
+$nome = $_POST['nome'];
+$cpf = $_POST['cpf'];
+$rg = $_POST['rg'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$genero = isset($_POST['genero']) ? $_POST['genero'] : '';
+$datanascimento = $_POST['datanascimento'];
+$pais = $_POST['pais'];
+$estado = $_POST['estado'];
+$cidade = $_POST['cidade'];
+$rua = $_POST['rua'];
+$numeroresi = $_POST['numeroresi'];
+$salario = $_POST['salario'];
+$valorapli = $_POST['valorapli'];
+
+$sql = "INSERT INTO usuarios(
+nome,
+cpf,
+rg,
+email,
+telefone,
+genero,
+datanascimento,
+pais,
+estado,
+cidade,
+rua,
+numeroresi,
+salario,
+valorapli
+)
+
+VALUES(
+'$nome',
+'$cpf',
+'$rg',
+'$email',
+'$telefone',
+'$genero',
+'$datanascimento',
+'$pais',
+'$estado',
+'$cidade',
+'$rua',
+'$numeroresi',
+'$salario',
+'$valorapli'
+)";
+
+if(mysqli_query($conexao, $sql)){
+    echo "Cadastro realizado com sucesso!";
+}else{
+    echo "Erro ao cadastrar!";
+}
+
+?>
